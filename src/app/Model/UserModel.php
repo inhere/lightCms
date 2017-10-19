22aea7536963dcb637c4cd18960ce2090b53b776
inhere <in.798@qq.com>
2017-10-19 18:06:58 +0800

    up

---
+++ src/app/Model/UserModel.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: inhere
+ * Date: 2017-10-19
+ * Time: 18:06
+ */
+
+namespace App\Model;
+
+/**
+ * Class UserModel
+ * @package App\Model
+ */
+class UserModel
+{
+
+}
